c07f216e8149adfdffb8496780e6bafd8efaebf5
Dion Moult <dion@thinkmoult.com>
2015-07-05 10:06:59 +1000

    Let there be code.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# sdk-php
-Makkoto SDK in PHP
+# makkoto-sdk-php
+
+Makkoto SDK in PHP.
+
+This is a tiny PSR-0 wrapper around the Guzzle HTTP Client (PSR-7).
+
+## More info
+
+ - http://sandbox.makkoto.com/doc/
---
+++ src/Makkoto/Sdk/Client.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Makkoto\Sdk;
+
+class Client
+{
+    private $makkoto_id;
+    private $client;
+    private $api_id;
+    private $api_secret;
+
+    public function __construct($makkoto_id)
+    {
+        $this->makkoto_id = $makkoto_id;
+
+        if ($this->makkoto_id === 'sandbox')
+        {
+            $base_uri = 'http://sandbox.makkoto.com';
+        }
+        else
+        {
+            $base_uri = 'http://baas.makkoto.com';
+        }
+
+        $this->client = new GuzzleHttp\Client(['base_uri' => $base_uri]);
+
+        if ( ! isset($_SESSION))
+        {
+            session_start();
+        }
+
+        if (isset($_SESSION['makkoto_api_id']))
+        {
+            $this->set_auth_details_from_session();
+        }
+    }
+
+    public function register(array $form_params)
+    {
+        return $this->request('post', 'user', $form_params);
+    }
+
+    public function login(array $form_params)
+    {
+        $get_api_details = $this->request('post', 'user/api', $form_params);
+        $api_details = json_decode($get_api_details->getBody());
+        $_SESSION['makkoto_api_id'] = $api_details->id;
+        $_SESSION['makkoto_api_secret'] = $api_details->secret;
+        $this->set_auth_details_from_session();
+        return $get_api_details;
+    }
+
+    public function request(
+        $method,
+        $resource,
+        array $form_params = array()
+    )
+    {
+        $body = array(
+            'headers' => array(
+                'Makkoto' => $this->makkoto_id,
+                'Accept' => 'application/hal+json;ver=1',
+                'Date' => date(DATE_RFC1123)
+            )
+        );
+
+        if ($this->api_id)
+        {
+            $body['headers']['Authentication'] = $this->get_auth_signature(
+                strtoupper($method), $resource, $this->api_id, $this->api_secret
+            );
+        }
+
+        if (isset($form_params['multipart']))
+        {
+            $body['multipart'] = $form_params['multipart'];
+        }
+        elseif ($form_params)
+        {
+            $body['form_params'] = $form_params;
+        }
+
+        $method = strtolower($method);
+        return $this->client->$method($resource, $body);
+    }
+
+    private function get_auth_signature($method, $resource, $api_id, $api_secret)
+    {
+        $method = strtoupper($method);
+        $canonical_payload = $method."\n".date(DATE_RFC1123).$resource;
+        $signature = base64_encode(
+            hash_hmac('sha1', $canonical_payload, $api_secret, TRUE)
+        );
+        return 'MAKKOTO '.$api_id.':'.$signature;
+    }
+
+    private function set_auth_details_from_session()
+    {
+        $this->api_id = $_SESSION['makkoto_api_id'];
+        $this->api_secret = $_SESSION['makkoto_api_secret'];
+    }
+}
